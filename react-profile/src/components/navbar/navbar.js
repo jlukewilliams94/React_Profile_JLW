b31e56639c5d77365e2c91ca0c15ec30be99aa2b
luke williams <jluke94williams@gmail.com>
2020-06-12 13:52:09 -0400

    Added navbar

--- react-profile/src/components/navbar/navbar.js
+++ react-profile/src/components/navbar/navbar.js
@@ -1,10 +1,19 @@
 import React from "react"
+//import Resume from "../images/"
 
 function Navbar() {
     return(
         <nav>
-            
+            <div className="nav-wrapper">
+                <a href="https://github.com/jlukewilliams94" className="brand-logo center">J Luke Williams Profile</a>
+                <ul id="nav-mobile" className="left hide-on-med-and-down">
+                    <li><a href="https://github.com/jlukewilliams94">GitHub</a></li>
+                    <li><a href="https://www.linkedin.com/in/jlukewilliams94">LinkedIn</a></li>
+                </ul>
+            </div>
         </nav>
 
     )
 }
+
+export default Navbar;
