ace6a0b2074e56bfd603a380e86b45cb4b67a647
luke williams <jluke94williams@gmail.com>
2020-06-17 21:34:48 -0400

    Changes navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -9,7 +9,7 @@ function Navbar() {
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
-            <a className="navbar-brand" title="Home">J.Luke Williams</a>
+            <h className="navbar-brand" title="Home">J.Luke Williams</h>
             <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
                 <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
                     <li className="nav-item active">
